fcb6e3483ae9d0bae8a1a5e3b0c549b34e136b70
mathu_lmn <mathurapha@gmail.com>
2022-10-24 11:13:18 +0200

    V 1.3.0 : Fixed cooldown, added remaining cooldown and ability to customize cooldown duration

--- server.lua
+++ server.lua
@@ -1,4 +1,5 @@
 local cooldowns = {}
+local cooldown = 60 -- the cooldown in seconds
 local max = 10 -- you can set here the maximum amount of peds you want to allow people on your server to spawn
 
 RegisterCommand('aggressive', function(source, args)
@@ -19,20 +20,31 @@ RegisterCommand('aggressive', function(source, args)
                 args = {"Aggressive-Ped", "^7You can only spawn up to ^1" .. max .. " ^7peds."}
               })
             return
-        end
-
-        if not cooldowns[source] or ((os.clock() - cooldowns[source]) > 60) then -- set a cooldown of 60 seconds to prevent spamming
-            cooldowns[source] = os.clock()
-            TriggerClientEvent('aggressiveCommand', source, amount)
-        
-        else -- If the player is on cooldown, notify the player
+        elseif cooldowns[source] then -- if the player is on cooldown, notify him
             TriggerClientEvent('chat:addMessage', source, {
                 color = { 255, 0, 0},
                 multiline = true,
-                args = {"Aggressive-Ped", "^7You can only use this command once every minute."}
+                args = {"Aggressive-Ped", "^7You can only use this command once every "..cooldown.." seconds. ("..cooldowns[source].." seconds left)"}
               })
+        else -- if all the checks are passed, execute the command
+            TriggerClientEvent('aggressiveCommand', source, amount)
+            cooldowns[source] = tonumber(cooldown) -- set the cooldown
         end
     else -- If the source is not a player, then notify the server console
         print("Failed to spawn peds (not a player).")
+    end
+end)
+
+-- each seconds, the cooldowns are reduced by 1 second
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        for k, v in pairs(cooldowns) do
+            if v > 0 then
+                cooldowns[k] = v - 1
+            else --remove the player from table if the cooldown is over
+                cooldowns[k] = nil
+            end
+        end
     end
 end)
